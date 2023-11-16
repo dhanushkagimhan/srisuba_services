0eec346418b7803e5915c315adeadd57da09c05a
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2023-11-16 13:17:17 +0530

    remove unwanted types

--- src/utility/types/index.ts
+++
@@ -1,9 +0,0 @@
-export enum ProposerStatus {
-    pendingEmailVerification = "PendingEmailVerification",
-    emailVerified = "EmailVerified",
-    pendingPayment = "PendingPayment",
-    paymentApproved = "PaymentApproved",
-    active = "Active",
-    reject = "Reject",
-    banned = "Banned",
-}
