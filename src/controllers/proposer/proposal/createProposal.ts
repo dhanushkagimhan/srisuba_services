65a4e5d7390ebd5b16420c95d406abe74bae4f01
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2023-11-20 21:50:36 +0530

    created proposalCreate API

--- src/controllers/proposer/proposal/createProposal.ts
+++ src/controllers/proposer/proposal/createProposal.ts
@@ -1,14 +1,51 @@
 import { type Request, type Response } from "express";
 import { type ValidationError, validationResult } from "express-validator";
 import prisma from "../../../utility/prismaClient/client";
-import { ProposerStatus } from "@prisma/client";
+import {
+    type FoodPreference,
+    ProposerStatus,
+    type Prisma,
+} from "@prisma/client";
 
 type RequestPayload = {
     profilePhoto: string;
+    otherPictures?: string[];
+    bioTitle?: string;
+    bioDescription?: string;
+    whatsAppNumber: string;
+    ethnicity: string;
+    religion: string;
+    caste?: string;
+    civilStatus: string;
+    height: string;
+    country: string;
+    stateOrDistrict: string;
+    city: string;
+    education: string;
+    profession: string;
+    drinking: boolean;
+    smoking: boolean;
+    foodPreference: FoodPreference;
+    fatherEthnicity: string;
+    fatherReligion: string;
+    fatherCaste?: string;
+    fatherProfession?: string;
+    fatherCountryOfResidence: string;
+    fatherAdditionalInfo?: string;
+    motherEthnicity: string;
+    motherReligion: string;
+    motherCaste?: string;
+    motherProfession?: string;
+    motherCountryOfResidence: string;
+    motherAdditionalInfo?: string;
+    horoscopeMatching: boolean;
 };
 
 type ApiResponse = {
     success: boolean;
+    data?: {
+        status: ProposerStatus;
+    };
     message?: string;
     errors?: ValidationError[];
 };
@@ -35,26 +72,70 @@ export const createProposal = async (
 
         console.log("res locals", res.locals.proposerId);
 
-        // const proposerUpdate = await prisma.proposer.update({
-        //     where: {
-        //         email: payload.email,
-        //     },
-        //     data: {
-        //         status: ProposerStatus.EmailVerified,
-        //     },
-        //     select: {
-        //         id: true,
-        //         status: true,
-        //     },
-        // });
+        const proposerId: number | undefined = res.locals.proposerId;
 
-        // console.log(
-        //     "proposer update {proposer-createProposal} : ",
-        //     proposerUpdate,
-        // );
+        if (proposerId == null) {
+            throw new Error("res local not have valid proposerId");
+        }
+
+        const proposer = await prisma.proposer.findUnique({
+            where: {
+                id: proposerId,
+            },
+            select: {
+                status: true,
+            },
+        });
+
+        console.log("found proposer data {createProposal} : ", proposer);
+
+        if (proposer == null) {
+            throw new Error("proposer data not found");
+        }
+
+        if (
+            proposer.status !== ProposerStatus.EmailVerified &&
+            proposer.status !== ProposerStatus.Reject
+        ) {
+            const responseData: ApiResponse = {
+                success: false,
+                message: "proposer status is not support to this request",
+            };
+            return res.status(403).send(responseData);
+        }
+
+        const proposerUpdatingData: Prisma.ProposerUpdateInput = {
+            ...payload,
+        };
+
+        if (proposer.status === ProposerStatus.EmailVerified) {
+            proposerUpdatingData.status = ProposerStatus.PendingPayment;
+        } else {
+            proposerUpdatingData.status = ProposerStatus.RejectionResolved;
+        }
+
+        const proposerUpdate = await prisma.proposer.update({
+            where: {
+                id: proposerId,
+            },
+            data: proposerUpdatingData,
+            select: {
+                id: true,
+                email: true,
+                status: true,
+            },
+        });
+
+        console.log(
+            "proposer update {proposer-createProposal} : ",
+            proposerUpdate,
+        );
 
         const responseData: ApiResponse = {
             success: true,
+            data: {
+                status: proposerUpdate.status,
+            },
         };
 
         return res.status(200).send(responseData);
--- src/utility/validations/proposer/proposal/createProposalValidation.ts
+++ src/utility/validations/proposer/proposal/createProposalValidation.ts
@@ -1,3 +1,4 @@
+import { FoodPreference } from "@prisma/client";
 import { checkExact, checkSchema } from "express-validator";
 
 export const createProposalValidation = checkExact(
@@ -10,7 +11,280 @@ export const createProposalValidation = checkExact(
             },
             isString: {
                 errorMessage: "profilePhoto should be string",
+            },
+        },
+        otherPictures: {
+            optional: true,
+            custom: {
+                options: async (otherPictures: string[]) => {
+                    if (otherPictures.length > 5) {
+                        throw new Error("can add 5 other pictures only");
+                    }
+                },
+            },
+        },
+        bioTitle: {
+            optional: true,
+            isString: {
+                errorMessage: "bioTitle should be string",
+            },
+        },
+        bioDescription: {
+            optional: true,
+            isString: {
+                errorMessage: "bioDescription should be string",
+            },
+        },
+        whatsAppNumber: {
+            exists: {
+                errorMessage: "whatsAppNumber is required",
+                options: { checkFalsy: true },
                 bail: true,
+            },
+            isString: {
+                errorMessage: "whatsAppNumber should be string",
+            },
+        },
+        ethnicity: {
+            exists: {
+                errorMessage: "ethnicity is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "ethnicity should be string",
+            },
+        },
+        religion: {
+            exists: {
+                errorMessage: "religion is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "religion should be string",
+            },
+        },
+        caste: {
+            optional: true,
+            isString: {
+                errorMessage: "caste should be string",
+            },
+        },
+        civilStatus: {
+            exists: {
+                errorMessage: "civilStatus is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "civilStatus should be string",
+            },
+        },
+        height: {
+            exists: {
+                errorMessage: "height is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "height should be string",
+            },
+        },
+        country: {
+            exists: {
+                errorMessage: "country is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "country should be string",
+            },
+        },
+        stateOrDistrict: {
+            exists: {
+                errorMessage: "stateOrDistrict is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "stateOrDistrict should be string",
+            },
+        },
+        city: {
+            exists: {
+                errorMessage: "profilePhoto is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "profilePhoto should be string",
+            },
+        },
+        education: {
+            exists: {
+                errorMessage: "profilePhoto is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "profilePhoto should be string",
+            },
+        },
+        profession: {
+            exists: {
+                errorMessage: "profilePhoto is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "profilePhoto should be string",
+            },
+        },
+        drinking: {
+            exists: {
+                errorMessage: "drinking is required",
+                bail: true,
+            },
+            isBoolean: {
+                errorMessage: "drinking should be boolean",
+            },
+        },
+        smoking: {
+            exists: {
+                errorMessage: "smoking is required",
+                bail: true,
+            },
+            isBoolean: {
+                errorMessage: "smoking should be boolean",
+            },
+        },
+        foodPreference: {
+            exists: {
+                errorMessage: "foodPreference is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "foodPreference should be string",
+            },
+            custom: {
+                options: (foodPreference) => {
+                    if (
+                        foodPreference !== FoodPreference.NonVegetarian &&
+                        foodPreference !== FoodPreference.Vegetarian &&
+                        foodPreference !== FoodPreference.Vegan
+                    ) {
+                        throw new Error("Invalid food preference");
+                    } else {
+                        return true;
+                    }
+                },
+            },
+        },
+        fatherEthnicity: {
+            exists: {
+                errorMessage: "fatherEthnicity is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "fatherEthnicity should be string",
+            },
+        },
+        fatherReligion: {
+            exists: {
+                errorMessage: "fatherReligion is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "fatherReligion should be string",
+            },
+        },
+        fatherCaste: {
+            optional: true,
+            isString: {
+                errorMessage: "fatherCaste should be string",
+            },
+        },
+        fatherProfession: {
+            optional: true,
+            isString: {
+                errorMessage: "fatherProfession should be string",
+            },
+        },
+        fatherCountryOfResidence: {
+            exists: {
+                errorMessage: "fatherCountryOfResidence is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "fatherCountryOfResidence should be string",
+            },
+        },
+        fatherAdditionalInfo: {
+            optional: true,
+            isString: {
+                errorMessage: "fatherAdditionalInfo should be string",
+            },
+        },
+        motherEthnicity: {
+            exists: {
+                errorMessage: "motherEthnicity is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "motherEthnicity should be string",
+            },
+        },
+        motherReligion: {
+            exists: {
+                errorMessage: "motherReligion is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "motherReligion should be string",
+            },
+        },
+        motherCaste: {
+            optional: true,
+            isString: {
+                errorMessage: "motherCaste should be string",
+            },
+        },
+        motherProfession: {
+            optional: true,
+            isString: {
+                errorMessage: "motherProfession should be string",
+            },
+        },
+        motherCountryOfResidence: {
+            exists: {
+                errorMessage: "motherCountryOfResidence is required",
+                options: { checkFalsy: true },
+                bail: true,
+            },
+            isString: {
+                errorMessage: "motherCountryOfResidence should be string",
+            },
+        },
+        motherAdditionalInfo: {
+            optional: true,
+            isString: {
+                errorMessage: "motherAdditionalInfo should be string",
+            },
+        },
+        horoscopeMatching: {
+            exists: {
+                errorMessage: "horoscopeMatching is required",
+                bail: true,
+            },
+            isBoolean: {
+                errorMessage: "horoscopeMatching should be boolean",
             },
         },
     }),
