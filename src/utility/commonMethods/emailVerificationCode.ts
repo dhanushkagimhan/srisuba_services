35bddc2e4a75b789d52347e39161fb1da0418dd7
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2023-12-03 11:31:30 +0530

    updated email verification code generation function

--- src/utility/commonMethods/emailVerificationCode.ts
+++ src/utility/commonMethods/emailVerificationCode.ts
@@ -2,25 +2,31 @@ import dayjs from "dayjs";
 import bcrypt from "bcrypt";
 
 const emailVerificationCode = async (): Promise<[string, string, Date]> => {
-    const saltRound = 8;
-    const emailVerifyCode: string = (Math.random() * 1000000)
-        .toPrecision(6)
-        .toString();
+    let emailVerifyCode: string;
+    for (let i = 0; i < 5; i++) {
+        emailVerifyCode = (Math.random() * 1000000).toPrecision(6);
 
-    const hashEmailVerification: string = await bcrypt.hash(
-        emailVerifyCode,
-        saltRound,
-    );
+        if (Number(emailVerifyCode) < 100000) {
+            continue;
+        }
+        const saltRound = 8;
+        const hashEmailVerification: string = await bcrypt.hash(
+            emailVerifyCode,
+            saltRound,
+        );
 
-    const emailVerificationCodeExpireTime: Date = dayjs()
-        .add(5, "minute")
-        .toDate();
+        const emailVerificationCodeExpireTime: Date = dayjs()
+            .add(5, "minute")
+            .toDate();
 
-    return [
-        emailVerifyCode,
-        hashEmailVerification,
-        emailVerificationCodeExpireTime,
-    ];
+        return [
+            emailVerifyCode,
+            hashEmailVerification,
+            emailVerificationCodeExpireTime,
+        ];
+    }
+
+    throw new Error("email verification code generation all 5 rounds failed");
 };
 
 export default emailVerificationCode;
